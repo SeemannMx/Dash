b2dd87571405df661ebf702832bbf617549234fa
Tino Kallinich <tino.kallinich-friedrich@inovex.de>
2020-02-26 18:07:23 +0100

    cleanup and added Homelist widget

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,10 +1,6 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_dash/screens/startscreen.dart';
-import 'package:flutter_dash/utils/provider.dart';
+import 'package:flutter_dash/screens/home_list.dart';
 
 class Homescreen extends StatefulWidget {
   static String route = "/home";
@@ -14,20 +10,7 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  StreamController _timeStreamController = StreamController();
-
-  Provider _provider = Provider();
-  Random _random = Random();
-  List<String> _chars = [];
-  List<Color> _colors = [
-    Colors.blueAccent,
-    Colors.blueAccent[100],
-    Colors.blue[200],
-    Colors.pinkAccent
-  ];
-  double _fontsize, _padding;
   Size _size;
-  Color color;
 
   @override
   Widget build(BuildContext context) {
@@ -37,96 +20,31 @@ class _HomescreenState extends State<Homescreen> {
     });
   }
 
-  @override
-  dispose() {
-    _provider.clear();
-    super.dispose();
+  _getDotText(String text, int limit) {
+    String result = "";
+    for (int i = 0; i < limit; i++) {
+      result += text;
+    }
+    return result;
   }
 
   _getBody() {
-    _provider.chatbot();
-
-    return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, Startscreen.route),
-      child: Container(
-        child: StreamBuilder<Object>(
-            stream: _timeStreamController.stream,
-            builder: (context, snapshot) {
-              _chars.clear();
-              _provider.run(_timeStreamController, 10, 2);
-
-              return (snapshot.data != null)
-                  ? _getContent(_getListBody(snapshot.data.toString()))
-                  : _getContent(_getProgress());
-            }),
-      ),
+    return Stack(
+      children: <Widget>[
+        _getStackItemText(),
+        _getStackItemList(),
+      ],
     );
   }
 
-  _getContent(Widget content) {
-    return AnimatedSwitcher(
-      duration: Duration(seconds: 1),
-      child: content,
+  _getStackItemText() {
+    return Text(_getDotText("010110011", 10000),
+        style: TextStyle(fontFamily: "Dot", color: Colors.white.withAlpha(50)));
+  }
+
+  _getStackItemList() {
+    return HomeList(
+      size: _size,
     );
-  }
-
-  _getListBody(String nr) {
-    for (int i = 0; i < nr.length; i++) _chars.add(nr[i]);
-
-    _fontsize = _getFontsize();
-    _padding = _fontsize * 0.25;
-
-    return Align(
-      alignment: Alignment.center,
-      child: Container(
-          height: _fontsize,
-          width: (_fontsize - _padding + (_padding * 0.2)) * _chars.length,
-          //color: Colors.black54,
-          child: _getList()),
-    );
-  }
-
-  _getProgress() {
-    return Center(child: CircularProgressIndicator());
-  }
-
-  _getList() {
-    return ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: _chars.length,
-        itemBuilder: (BuildContext context, int index) {
-          return _getListItem(index);
-        });
-  }
-
-  _getListItem(int index) {
-    return Container(
-      decoration: BoxDecoration(
-          color: _getColor(index),
-          borderRadius: BorderRadius.all(Radius.circular(_padding / 2)),
-          border: Border.all(color: Colors.pinkAccent, width: 1)),
-      alignment: Alignment.center,
-      padding: EdgeInsets.symmetric(horizontal: _padding),
-      child: Text(
-        _chars[index],
-        style: TextStyle(fontSize: _fontsize / 2),
-      ),
-    );
-  }
-
-  _getFontsize() {
-    return double.parse(_chars[0]) < 8
-        ? double.parse(_chars[0]) * 50
-        : this._size.height / 2;
-  }
-
-  _getColor(int index) {
-    index = _random.nextInt(_colors.length);
-    int last = index;
-
-    if (index == last && index == _colors.length) index - 1;
-    if (index == last && index == 0) index + 1;
-
-    return _colors[index];
   }
 }
--- lib/utils/provider.dart
+++ lib/utils/provider.dart
@@ -1,18 +1,31 @@
 import 'dart:convert';
 import 'dart:async';
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
 class Provider {
   Provider._internal() {}
   static Provider _provider = Provider._internal();
 
+  Random _random = Random();
   Timer _timer;
+
   var _value;
-  int _runner = 0;
+  var _runner = 0;
 
   // URL
   String url_numbers = "http://numbersapi.com/random/math?json";
   String url = "http://localhost:5005";
+
+  // Colors
+  List<Color> _colors = [
+    Colors.blueAccent,
+    Colors.blueAccent[100],
+    Colors.blue[200],
+    Colors.pinkAccent
+  ];
 
   factory Provider({var p}) => _provider;
 
@@ -52,5 +65,15 @@ class Provider {
   chatbot() async {
     //print("send request to rasa $url");
     //await get(url).then((response) => print(response.body));
+  }
+
+  getColor(int index) {
+    index = _random.nextInt(_colors.length);
+    int last = index;
+
+    if (index == last && index == _colors.length) index - 1;
+    if (index == last && index == 0) index + 1;
+
+    return _colors[index];
   }
 }
